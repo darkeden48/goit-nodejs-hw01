ddf134468891e503cd768c8369192bb5ffbe0df9
darkeden48 <dzalex72@list.ru>
2022-05-29 15:21:07 +0300

    save

---
+++ contacts.js
@@ -0,0 +1,50 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const listContacts = async()=> {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  };
+  
+ const getContactById = async(contactId)=> {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId);
+    if(!result){
+        return null;
+    }
+    return result;
+  };
+  
+  const removeContact = async(contactId)=> {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item => item.id === contactId);
+    if(idx === -1){
+        return null;
+    }
+    const [removedContacts] = contacts.splice(idx, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return removedContacts;
+    // const newСontacts = contacts.filter((_, index) => index !== idx);
+    // updateProducts(newСontacts);
+    // return contacts[idx];
+  };
+  
+  const addContact = async(data)=> {
+    const contacts = await listContacts();
+    const newСontact = {id: nanoid(),...data};
+    contacts.push(newСontact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newСontact;
+  };
+
+    module.exports = {
+        listContacts,
+        getContactById,
+        removeContact,
+        addContact,
+    };
+    
